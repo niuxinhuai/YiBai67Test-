9abf29214b82e2881853660b493624a26f5757c8
shengWei <tosongxianxian@163.com>
2016-02-16 14:06:46 +0800

    Initial Commit

---
+++ YiBai67Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YiBai67Test
+//
+//  Created by songxianxian on 16/2/16.
+//  Copyright (c) 2016年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
